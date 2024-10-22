146101682c4f9cbc461d3c1b217861a995f68d7e
velazquezxana597@gmail.com <velazquezxana597@gmail.com>
2024-10-21 21:47:17 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_implementacion_XGVC1/VistaModelo/VMpatron.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_implementacion_XGVC1.VistaModelo
+{
+    internal class VMpatron : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONSTRUCTOR
+
+        #endregion
+        #region OBJETOS
+
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+    }
+}
