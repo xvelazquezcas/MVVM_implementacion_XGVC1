96b405fbd604fa32494fd623dce9aa0dcee70190
velazquezxana597@gmail.com <velazquezxana597@gmail.com>
2024-11-07 22:00:36 -0700

    2.3

--- MVVM_implementacion_XGVC1/VistaModelo/VMpagina1.cs
+++ MVVM_implementacion_XGVC1/VistaModelo/VMpagina1.cs
@@ -10,35 +10,58 @@ namespace MVVM_implementacion_XGVC1.VistaModelo
     internal class VMpagina1 : BaseViewModel
     {
         #region VARIABLES
-        string _Texto;
+        string _N1;
+        string _N2;
+        string _R;
         #endregion
         #region CONSTRUCTOR
         public VMpagina1(INavigation navigation)
         {
-             Navigation = navigation;
+            Navigation = navigation;
         }
         #endregion
         #region OBJETOS
 
-        public string Texto
+        public string N1
         {
-            get { return _Texto; }
-            set { SetValue(ref _Texto, value); }
+            get { return _N1; }
+            set { SetValue(ref _N1, value); }
+        }
+        public string N2
+        {
+            get { return _N2; }
+            set { SetValue(ref _N2, value); }
+        }
+        public string R
+        {
+            get { return _R; }
+            set { SetValue(ref _R, value); }
         }
         #endregion
         #region PROCESOS
-        public async Task Alerta()
-        {
-            await DisplayAlert("Titulo", "Mensaje", "Okeyys");
-        }
-        public void ProcesoSimple()
+        public async Task Procesoasync()
         {
 
         }
+
+        public void Sumar()
+        {
+            double n1 = 0;
+            double n2 = 0;
+            double r = 0;
+
+            n1 = Convert.ToDouble(N1);
+            n2 = Convert.ToDouble(N2);
+            r = Convert.ToDouble(R);
+
+            r = n1 + n2;
+            R = r.ToString();
+        }
+        
         #endregion
         #region COMANDOS
-        public ICommand AlertaCommand => new Command(async () => await Alerta());
-        public ICommand procesoSimpcommand => new Command(ProcesoSimple);
+        public ICommand ProcesoasyncCommand => new Command(async () => await Procesoasync());
+        public ICommand Suymarcommand => new Command(Sumar);
         #endregion
     }
 }
